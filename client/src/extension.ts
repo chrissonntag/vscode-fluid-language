799d69423d7d95714aed59ac5a91faa7bd5cd773
Simon Praetorius <simon@praetorius.me>
2026-04-03 13:12:12 +0200

    [FEATURE] Provide basic validation by calling Fluid CLI

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,8 +1,32 @@
+import { spawnSync } from 'child_process';
 import { hash } from 'crypto';
 import type { CompletionItem, ExtensionContext, ProviderResult, Hover } from 'vscode';
 import * as vscode from 'vscode';
 
-export function activate(context: ExtensionContext) {
+let diagnosticCollection: vscode.DiagnosticCollection;
+let fluidBinary;
+
+export function activate(ctx: ExtensionContext) {
+	fluidBinary = vscode.workspace.getConfiguration('fluid').get('fluidBinary');
+
+	if (fluidBinary !== '') {
+		diagnosticCollection = vscode.languages.createDiagnosticCollection('fluid');
+		ctx.subscriptions.push(diagnosticCollection);
+		ctx.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(editor => {
+			if (editor) {
+				updateDiagnostics(editor.document, diagnosticCollection);
+			}
+		}));
+		ctx.subscriptions.push(vscode.workspace.onDidChangeTextDocument(e => {
+			updateDiagnostics(e.document, diagnosticCollection);
+		}));
+		if (vscode.window.activeTextEditor) {
+			updateDiagnostics(vscode.window.activeTextEditor.document, diagnosticCollection);
+		}
+	} else {
+		console.log('Skipping file validation because Fluid CLI path is not set in configuration.');
+	}
+
 	const virtualDocumentContents = new Map<string, string>();
 
 	vscode.workspace.registerTextDocumentContentProvider('embedded-content', {
@@ -15,7 +39,6 @@ export function activate(context: ExtensionContext) {
 	vscode.languages.registerHoverProvider('html-fluid', {
 		async provideHover(document, position, token) {
 			const embeddedContentIdentifier = hash('sha256', document.uri.toString(true)) + '.html';
-			console.log('provideHover ' + embeddedContentIdentifier);
 			virtualDocumentContents.set(embeddedContentIdentifier, document.getText());
 			const vdocUriString = `embedded-content://html/${encodeURIComponent(embeddedContentIdentifier)}`;
 			const vdocUri = vscode.Uri.parse(vdocUriString);
@@ -31,7 +54,6 @@ export function activate(context: ExtensionContext) {
 	vscode.languages.registerCompletionItemProvider('html-fluid', {
 		async provideCompletionItems(document, position, token, context) {
 			const embeddedContentIdentifier = hash('sha256', document.uri.toString(true)) + '.html';
-			console.log('provideCompletionItems ' + embeddedContentIdentifier);
 			virtualDocumentContents.set(embeddedContentIdentifier, document.getText());
 			const vdocUriString = `embedded-content://html/${encodeURIComponent(embeddedContentIdentifier)}`;
 			const vdocUri = vscode.Uri.parse(vdocUriString);
@@ -43,4 +65,36 @@ export function activate(context: ExtensionContext) {
 			);
 		},
 	});
+}
+
+function updateDiagnostics(document: vscode.TextDocument, collection: vscode.DiagnosticCollection): void {
+	if (fluidBinary === '') {
+		return;
+	}
+	if (document.languageId !== 'fluid' && document.languageId !== 'html-fluid') {
+		return;
+	}
+	const diagnostics = [];
+	let result;
+	try {
+		const fluidCli = spawnSync(vscode.workspace.getWorkspaceFolder(document.uri).uri.fsPath + "/" + fluidBinary, ['analyze'], { input: document.getText() });
+		result = JSON.parse(fluidCli.stdout.toString());
+	} catch (err) {
+		console.error(err);
+		return;
+	}
+	result.errors.forEach(error => {
+		const [, line, character, message] = error.message.match(
+			/Fluid parse error in template .*, line ([0-9]+) at character ([0-9]+). Error: (.*?)(?: Template source chunk:|$)/
+		);
+		const position = (line && character) ? new vscode.Position(Number(line) - 1, Number(character) - 1) : new vscode.Position(0, 0);
+		const diagnostic = new vscode.Diagnostic(new vscode.Range(position, position), message ?? error.message, vscode.DiagnosticSeverity.Error);
+		diagnostics.push(diagnostic);
+	});
+	result.deprecations.forEach(deprecation => {
+		const position = new vscode.Position(Number(deprecation.line) - 1, 0);
+		const diagnostic = new vscode.Diagnostic(new vscode.Range(position, position), deprecation.message, vscode.DiagnosticSeverity.Information);
+		diagnostics.push(diagnostic);
+	});
+	collection.set(document.uri, diagnostics);
 }
