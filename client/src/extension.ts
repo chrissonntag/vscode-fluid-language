6d6e61bda4f2d97d4e4c52b34c82793362bba1c6
Simon Praetorius <simon@praetorius.me>
2026-04-06 13:52:45 +0200

    [TASK] Apply editorconfig

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -6,79 +6,79 @@ let fluidBinary, typo3Binary;
 let diagnosticCollection: DiagnosticCollection;
 
 export function activate(ctx: ExtensionContext) {
-	setupFluidBinary(vscode.workspace.getConfiguration('fluid.bin').get('fluid'));
-	setupTypo3Binary(vscode.workspace.getConfiguration('fluid.bin').get('typo3'));
-	ctx.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
-		if (e.affectsConfiguration('fluid.bin')) {
-			setupFluidBinary(vscode.workspace.getConfiguration('fluid.bin').get('fluid'));
-			setupTypo3Binary(vscode.workspace.getConfiguration('fluid.bin').get('typo3'));
-		}
-	}));
+    setupFluidBinary(vscode.workspace.getConfiguration('fluid.bin').get('fluid'));
+    setupTypo3Binary(vscode.workspace.getConfiguration('fluid.bin').get('typo3'));
+    ctx.subscriptions.push(vscode.workspace.onDidChangeConfiguration(e => {
+        if (e.affectsConfiguration('fluid.bin')) {
+            setupFluidBinary(vscode.workspace.getConfiguration('fluid.bin').get('fluid'));
+            setupTypo3Binary(vscode.workspace.getConfiguration('fluid.bin').get('typo3'));
+        }
+    }));
 
-	if (fluidBinary !== '') {
-		diagnosticCollection = vscode.languages.createDiagnosticCollection('fluid');
-		ctx.subscriptions.push(diagnosticCollection);
-		ctx.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(editor => {
-			if (editor) {
-				updateDiagnostics(editor.document, diagnosticCollection);
-			}
-		}));
-		ctx.subscriptions.push(vscode.workspace.onDidChangeTextDocument(e => {
-			updateDiagnostics(e.document, diagnosticCollection);
-		}));
-		if (vscode.window.activeTextEditor) {
-			updateDiagnostics(vscode.window.activeTextEditor.document, diagnosticCollection);
-		}
-	} else {
-		console.log('Skipping file validation because Fluid CLI path is not set in configuration.');
-	}
+    if (fluidBinary !== '') {
+        diagnosticCollection = vscode.languages.createDiagnosticCollection('fluid');
+        ctx.subscriptions.push(diagnosticCollection);
+        ctx.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(editor => {
+            if (editor) {
+                updateDiagnostics(editor.document, diagnosticCollection);
+            }
+        }));
+        ctx.subscriptions.push(vscode.workspace.onDidChangeTextDocument(e => {
+            updateDiagnostics(e.document, diagnosticCollection);
+        }));
+        if (vscode.window.activeTextEditor) {
+            updateDiagnostics(vscode.window.activeTextEditor.document, diagnosticCollection);
+        }
+    } else {
+        console.log('Skipping file validation because Fluid CLI path is not set in configuration.');
+    }
 }
 
 function setupFluidBinary(userInput: string): void {
-	fluidBinary = '';
-	if (userInput !== '') {
-		// TODO verify that the required commands are available
-		fluidBinary = userInput;
-	}
+    fluidBinary = '';
+    if (userInput !== '') {
+        // TODO verify that the required commands are available
+        fluidBinary = userInput;
+    }
 }
 
 function setupTypo3Binary(userInput: string): void {
-	typo3Binary = '';
-	if (userInput !== '') {
-		// TODO verify that the required commands are available
-		typo3Binary = userInput;
-	}
+    typo3Binary = '';
+    if (userInput !== '') {
+        // TODO verify that the required commands are available
+        typo3Binary = userInput;
+    }
 }
 
 function updateDiagnostics(document: TextDocument, collection: DiagnosticCollection): void {
-	if (fluidBinary === '') {
-		return;
-	}
-	if (document.languageId !== 'fluid' && document.languageId !== 'html-fluid') {
-		return;
-	}
-	const workspacePath = vscode.workspace.getWorkspaceFolder(document.uri).uri.fsPath + '/';
-	const diagnostics = [];
-	let result;
-	try {
-		const fluidCli = spawnSync(workspacePath + fluidBinary, ['analyze', '--json', '--stdin'], { input: document.getText() });
-		result = JSON.parse(fluidCli.stdout.toString());
-	} catch (err) {
-		console.error(err);
-		return;
-	}
-	result.errors.forEach(error => {
-		const [, line, character, message] = error.message.match(
-			/Fluid parse error in template .*, line ([0-9]+) at character ([0-9]+). Error: (.*?)(?: Template source chunk:|$)/
-		);
-		const position = (line && character) ? new vscode.Position(Number(line) - 1, Number(character) - 1) : new vscode.Position(0, 0);
-		const diagnostic = new vscode.Diagnostic(new vscode.Range(position, position), message ?? error.message, vscode.DiagnosticSeverity.Error);
-		diagnostics.push(diagnostic);
-	});
-	result.deprecations.forEach(deprecation => {
-		const position = new vscode.Position(Number(deprecation.line) - 1, 0);
-		const diagnostic = new vscode.Diagnostic(new vscode.Range(position, position), deprecation.message, vscode.DiagnosticSeverity.Information);
-		diagnostics.push(diagnostic);
-	});
-	collection.set(document.uri, diagnostics);
+    if (fluidBinary === '') {
+        return;
+    }
+    if (document.languageId !== 'fluid' && document.languageId !== 'html-fluid') {
+        return;
+    }
+    const workspacePath = vscode.workspace.getWorkspaceFolder(document.uri).uri.fsPath + '/';
+    const diagnostics = [];
+    let result;
+    try {
+        const fluidCli = spawnSync(workspacePath + fluidBinary, ['analyze', '--json', '--stdin'], { input: document.getText() });
+        result = JSON.parse(fluidCli.stdout.toString());
+    } catch (err) {
+        console.error(err);
+        return;
+    }
+    result.errors.forEach(error => {
+        const [, line, character, message] = error.message.match(
+            /Fluid parse error in template .*, line ([0-9]+) at character ([0-9]+). Error: (.*?)(?: Template source chunk:|$)/
+        );
+        const position = (line && character) ? new vscode.Position(Number(line) - 1, Number(character) - 1) : new vscode.Position(0, 0);
+        const diagnostic = new vscode.Diagnostic(new vscode.Range(position, position), message ?? error.message, vscode.DiagnosticSeverity.Error);
+        diagnostics.push(diagnostic);
+    });
+    result.deprecations.forEach(deprecation => {
+        const position = new vscode.Position(Number(deprecation.line) - 1, 0);
+        const diagnostic = new vscode.Diagnostic(new vscode.Range(position, position), deprecation.message, vscode.DiagnosticSeverity.Information);
+        diagnostics.push(diagnostic);
+    });
+    collection.set(document.uri, diagnostics);
 }
--- eslint.config.mts
+++ eslint.config.mts
@@ -5,8 +5,8 @@ import json from "@eslint/json";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(["fluid/.Build/", "client/out/"]),
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  { files: ["**/*.json"], ignores: ["**/package-lock.json"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+    globalIgnores(["fluid/.Build/", "client/out/"]),
+    { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+    tseslint.configs.recommended,
+    { files: ["**/*.json"], ignores: ["**/package-lock.json"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
 ]);
